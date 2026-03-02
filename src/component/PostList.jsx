d900c0ef318faa55653abb15b18b48cc22b75c50
tausif900 <ansaritausif9268@gmail.com>
2026-03-02 12:06:07 +0530

    layout

---
+++ src/component/PostList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post'
+
+const PostList = () => {
+    return (
+        <div>
+            <Post />
+            <Post />
+            <Post />
+        </div>
+    )
+}
+
+export default PostList
